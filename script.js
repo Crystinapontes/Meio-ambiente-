7ceb83174dfa20389f9485fde4cb725830dcabcb
Crystinapontes <146957310+Crystinapontes@users.noreply.github.com>
2025-07-02 11:31:15 -0300

    .

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "perguntas sobre a degradação do Meio ambiente",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
